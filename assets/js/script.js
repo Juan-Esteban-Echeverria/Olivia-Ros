614da1282afd85a12297530877b8e04d83e31911
Juan-Esteban-Echeverria <jeecheverria@uc.cl>
2021-09-28 14:04:06 -0300

    añadiendo javascript

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $("a").click(function(event){
+        if (this.hash !== ""){
+            event.preventDefault();
+
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 10, function(){
+                window.location.hash = gato;
+            });
+        }
+    });
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+});
